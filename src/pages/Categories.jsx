461480b83b3d97955353090ca68d26a9672fd7b4
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-03 00:43:26 +0000

    Add button to Categories page

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Categories = () => (
-  <div>Categories</div>
+  <div className="p-3 md:px-16 md:py-5">
+    <button
+      type="button"
+      className="bg-slate-400  p-2 uppercase text-sm rounded"
+    >
+      Check status
+    </button>
+  </div>
 );
 
 export default Categories;
