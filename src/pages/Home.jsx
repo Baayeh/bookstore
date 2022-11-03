61da511e9f6d759748f80553f6eca3877b18c963
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-03 00:36:45 +0000

    Add Book and Navbar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Book from '../components/Book';
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+  <section className="p-3 md:px-16 md:py-5">
+    <div className="book-list">
+      <Book title="House of Dragons" author="George R. R. Martin" />
+    </div>
+  </section>
+);
 
 export default Home;
