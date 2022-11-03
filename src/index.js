2289f5521f90d32074e5b2b55a2267410cac564c
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-03 00:33:56 +0000

    Add Home page to display list of books

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Categories from './pages/Categories';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'categories',
+        element: <Categories />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
