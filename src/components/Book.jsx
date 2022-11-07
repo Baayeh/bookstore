5a521552d092646b61b6cda534cf7607b82af94a
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-07 19:06:06 +0000

    Implement Remove Book action

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { useDispatch } from 'react-redux';
-// import { removeBook } from '../redux/books/books';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
-const Book = ({ book }) => (
-  // const dispatch = useDispatch();
+const Book = ({ book }) => {
+  const dispatch = useDispatch();
 
-  // const deleteBook = () => {
-  //   dispatch(removeBook(book.id));
-  // };
+  const deleteBook = () => {
+    dispatch(removeBook(book.id));
+  };
 
-  <div className="book-card border bg-white p-3 shadow rounded">
-    <small>Category</small>
-    <h2 className="title text-blue-900 font-extrabold text-xl">{book.title}</h2>
-    <p className="author">{book.author}</p>
-    <button
-      type="button"
-      className="p-2 text-white mt-3 bg-blue-600 rounded"
-      // onClick={deleteBook}
-    >
-      Remove
-    </button>
-  </div>
-);
+  return (
+    <div className="book-card border bg-white p-3 shadow rounded">
+      <small>Category</small>
+      <h2 className="title text-blue-900 font-extrabold text-xl">
+        {book.title}
+      </h2>
+      <p className="author">{book.author}</p>
+      <button
+        type="button"
+        className="p-2 text-white mt-3 bg-blue-600 rounded"
+        onClick={deleteBook}
+      >
+        Remove
+      </button>
+    </div>
+  );
+};
+
 Book.propTypes = {
   book: PropTypes.shape({
     id: PropTypes.string,
