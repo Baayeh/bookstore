c11a57cd8715e76816b82a24a0878a3e19ff91df
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-08 17:27:12 +0000

    Add final touches

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,40 +1,57 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
+import { ProgressSpinner } from 'primereact/progressspinner';
 import { addBook } from '../redux/books/bookSlice';
 
-const AddBook = () => {
+const AddBook = ({ entity, toast }) => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('Fiction');
+  const [status, setStatus] = useState(true);
   const dispatch = useDispatch();
 
-  const scrollToTop = () => {
-    window.scrollTo(0, 0);
+  const selectCategory = (e) => {
+    setCategory(e.target.value);
   };
 
   const addABook = () => {
+    if (title === '' || author === '') {
+      toast.fire({
+        icon: 'error',
+        title: 'All fields are required to add a book',
+      });
+    }
+
     if (title !== '' && author !== '') {
+      setStatus(false);
       const book = {
         item_id: uuidv4(),
         title,
         author,
-        category: 'Sci-fi',
+        category,
       };
       dispatch(addBook(book));
-      scrollToTop();
       setTitle('');
       setAuthor('');
     }
   };
 
+  useEffect(() => {
+    if (entity.msg) {
+      setStatus(true);
+    }
+  }, [entity.msg]);
+
   return (
-    <section id="form-section" className="mt-5">
-      <h3>Add New Book</h3>
-      <form className="flex flex-col md:flex-row gap-5">
-        <div className="form-control flex gap-5">
+    <section id="form-section" className="mt-10 border-t-2 pt-5">
+      <h3 className="text-2xl font-bold">Add New Book</h3>
+      <form className="">
+        <div className="form-control grid md:grid-cols-3 lg:grid-cols-4 gap-x-5 gap-y-5">
           <input
             type="text"
-            className="p-2 border focus:outline-none w-[12.5rem] md:w-[27rem] lg:w-[35rem]"
+            className="p-3 md:p-2 border focus:outline-none col-span-1"
             placeholder="Book title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
@@ -42,25 +59,53 @@ const AddBook = () => {
           />
           <input
             type="text"
-            className="p-2 border focus:outline-none w-[12.5rem] md:w-[27rem] lg:w-[35rem]"
+            className="p-3 md:p-2 border focus:outline-none"
             placeholder="Author"
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
             required
           />
-        </div>
-        <div className="form-action">
-          <button
-            type="button"
-            className="uppercase bg-blue-600 text-white p-2 rounded md:w-[9rem]"
-            onClick={addABook}
+          <select
+            name="category"
+            id="category"
+            className="p-3 md:p-2 border focus:outline-none"
+            value={category}
+            onChange={selectCategory}
           >
-            Add Book
-          </button>
+            <option value="Fiction">Fiction</option>
+            <option value="Non Fiction">Non Fiction</option>
+          </select>
+
+          <div className="form-action">
+            {status && (
+              <button
+                type="button"
+                className="uppercase bg-blue-600 text-white p-2 rounded md:w-[9rem]"
+                onClick={addABook}
+              >
+                Add Book
+              </button>
+            )}
+            {!status && (
+              <button
+                type="button"
+                className="uppercase bg-blue-600 text-white p-2 rounded md:w-[9rem] flex justify-center gap-2 items-center"
+              >
+                <span>
+                  <ProgressSpinner className="w-5 h-5" strokeWidth={5} />
+                </span>
+                Adding...
+              </button>
+            )}
+          </div>
         </div>
       </form>
     </section>
   );
 };
 
+AddBook.propTypes = {
+  entity: PropTypes.shape().isRequired,
+  toast: PropTypes.func.isRequired,
+};
 export default AddBook;
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,29 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
 import { deletedBook } from '../redux/books/bookSlice';
 
 const Book = ({ book }) => {
   const dispatch = useDispatch();
+  const [percent, setPercent] = useState(0);
 
   const deleteBook = () => {
     dispatch(deletedBook(book.item_id));
   };
 
+  const numbers = [];
+
+  const storeNumbers = () => {
+    let i = 0;
+    // eslint-disable-next-line no-plusplus
+    for (i; i <= 100; i++) {
+      numbers.push(i);
+    }
+    return numbers;
+  };
+
+  storeNumbers();
+
+  useEffect(() => {
+    const random = numbers[Math.floor(Math.random() * numbers.length)];
+    setPercent(random);
+  }, []);
+
   return (
-    <div className="book-card border bg-white p-3 shadow rounded">
-      <small>{book.category}</small>
-      <h2 className="title text-blue-900 font-extrabold text-xl">
-        {book.title}
-      </h2>
-      <p className="author">{book.author}</p>
-      <button
-        type="button"
-        className="p-2 text-white mt-3 bg-blue-600 rounded"
-        onClick={deleteBook}
-      >
-        Remove
-      </button>
+    <div className="book-card border bg-white p-8 shadow-sm rounded flex items-center justify-between">
+      <div>
+        <p className="text-gray-400 font-bold">{book.category}</p>
+        <h1 className="title text-slate-700 font-extrabold text-2xl">
+          {book.title}
+        </h1>
+        <p className="author text-blue-700 font-light text-lg">{book.author}</p>
+        <div className="actions mt-5">
+          <button
+            type="button"
+            className="pr-2 text-blue-700 text-sm hover:text-blue-900"
+          >
+            Comments
+          </button>
+          <button
+            type="button"
+            className="px-2 text-blue-700 border-l-2 border-r-2 text-sm hover:text-blue-900"
+            onClick={deleteBook}
+          >
+            Remove
+          </button>
+          <button
+            type="button"
+            className="px-2 text-blue-700 text-sm hover:text-blue-900"
+          >
+            Edit
+          </button>
+        </div>
+      </div>
+
+      <div className="md:flex gap-16 hidden">
+        <div className="flex items-center gap-4">
+          <div className="w-20 h-20">
+            <CircularProgressbar value={percent} />
+          </div>
+          <div className="flex flex-col items-center text-gray-400">
+            <p className="text-2xl">
+              {percent}
+              %
+            </p>
+            <span>completed</span>
+          </div>
+        </div>
+
+        <div className="border-l-2 pl-10">
+          <p className="uppercase font-light text-gray-500">Current Chapter</p>
+          <h3>Chapter 3: A Lesson Learned</h3>
+          <button
+            type="button"
+            className="px-4 py-2 bg-blue-500 text-white rounded text-sm mt-5 uppercase"
+          >
+            Update Progress
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
